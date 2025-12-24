b77064d726a1d85d0da6a6674fbf0f208afe8e49
Dheeraj Gowda YS <dheeraj76766@gmail.com>
2025-12-24 14:41:44 +0530

    .

--- hello.java
+++ hello.java
@@ -2,8 +2,11 @@ public class hello
 {
     public static void main (String args[])
     {
-        int num[] = {5,6,7};
-        num[1] = 1;
-        System.out.println(num[1]);
+        int num[] ={5,6,7,8,9,10};
+        
+        for(int i=0;i<3;i++)
+        {
+        System.out.println(num[i]);
+        }
     }
 }

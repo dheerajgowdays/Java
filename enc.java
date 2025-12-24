b2f1b2fa94afd55fdbb47a06ebe50e546250324b
Dheeraj Gowda YS <dheeraj76766@gmail.com>
2025-12-24 23:14:45 +0530

    java practice

---
+++ enc.java
@@ -0,0 +1,31 @@
+class humans{
+    private String name;
+    private int age;
+
+    public void setName(String n)
+    {
+        name=n;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public void setAge(int a)
+    {
+        age =  a;
+    }
+    public int getAge()
+    {
+        return age;
+    }
+}
+public class enc {
+    public static void main (String args[])
+    {
+        humans obj = new humans();
+        obj.setAge(20);
+        obj.setName("Dheeraj");
+        System.out.println(obj.getName()+":"+obj.getAge());
+    }
+    
+}
---
+++ staticm.java
@@ -0,0 +1,25 @@
+class Mobile{
+    String brand;
+    int price;
+    static String name;
+
+    public static void show(Mobile obj){
+        System.out.println(obj.brand+":"+obj.price+":"+ name);
+    }
+}
+public class staticm {
+    public static void main(String args[])
+    {
+        Mobile obj1 = new Mobile();
+        obj1.brand = "apple";
+        obj1.price = 1500;
+        Mobile.name = "Phone";
+
+        Mobile obj2 = new Mobile();
+        obj2.brand = "Oneplus";
+        obj2.price = 150;
+
+        Mobile.show(obj1);
+    }
+    
+}

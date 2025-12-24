627a0d101e26e9867d7926f3886366ac80bfdc8c
Dheeraj Gowda YS <dheeraj76766@gmail.com>
2025-12-24 20:29:08 +0530

    .

---
+++ staticv.java
@@ -0,0 +1,31 @@
+class Mobile{
+    String brand ;
+    int price;
+    static String name;
+
+    public void show()
+    {
+        System.out.println(brand+":"+price+":"+name);
+    }
+}
+
+public class staticv {
+    public static void main(String args[])
+    {
+        Mobile obj1 = new Mobile();
+        obj1.brand="Apple";
+        obj1.price=1500;
+        Mobile.name = "SmartPhone";
+
+        Mobile obj2 = new Mobile();
+        obj2.brand="Oneplus";
+        obj2.price=150;
+        Mobile.name="SmartPhone";
+
+        Mobile.name="Phone";
+
+        obj1.show();
+        obj2.show();
+    }
+    
+}

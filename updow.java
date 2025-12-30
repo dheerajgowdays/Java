4443fce5a30e2679e1dabe8683160354a772e516
Dheeraj Gowda YS <dheeraj76766@gmail.com>
2025-12-30 23:27:39 +0530

    casting

---
+++ updow.java
@@ -0,0 +1,21 @@
+class A{
+    public void show1(){
+        System.out.println("in A show");
+    }
+}
+class B extends A{
+    public void show2(){
+        System.out.println("in B show");
+    }
+}
+public class updow {
+    public static void main (String args[])
+    {
+        A obj = (A) new B();  // up casting
+        obj.show1();
+
+        B obj1 = (B) obj; //down casting 
+        obj1.show2();
+    }
+    
+}

cb71450323a5c1110e506374be9b2e196eff105d
Dheeraj Gowda YS <dheeraj76766@gmail.com>
2026-01-03 01:11:08 +0530

    Exception

---
+++ excepthand.java
@@ -0,0 +1,23 @@
+public class excepthand {
+    public static void main (String args[])
+    {
+        int i=0;
+        int j=0;
+
+        try{
+            j=20/i;
+            if (j==0){
+                throw new ArithmeticException ("zero should not be answer");
+            }
+        }
+        catch (ArithmeticException e){
+            j=20/1;
+            System.out.println("thats the default output " + e);
+        }
+        catch (Exception  e){
+            System.out.println("Something went wrong " + e);
+        }
+        System.out.println(j);
+    }
+    
+}

ab252bbb711962dff74c6d13e0557e6cc9334a0a
Dheeraj Gowda YS <dheeraj76766@gmail.com>
2026-01-02 23:07:35 +0530

    functional interface

---
+++ sam.java
@@ -0,0 +1,14 @@
+@FunctionalInterface
+interface A{
+    int add(int i,int j);
+}
+
+public class sam {
+    public static void main(String args[])
+    {
+        A obj = (i,j) -> i+j;
+
+        int result = obj.add(4,5);
+        System.out.println(result);
+    }
+}

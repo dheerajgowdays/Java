11719e715627690e6b7eb173e52ac48d93cf17bd
Dheeraj Gowda YS <dheeraj76766@gmail.com>
2026-01-05 01:48:08 +0530

    simple

---
+++ GeeksforGeeks/gfg.java
@@ -0,0 +1,10 @@
+package GeeksforGeeks;
+import java.util.Scanner;
+public class gfg {
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter  a number:");
+        int num = sc.nextInt();
+        System.out.println("Number is: "+num);
+    }
+}

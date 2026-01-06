2b33dfaa11e7bc6aa4e63d47099dc078eaf97e4e
Dheeraj Gowda YS <dheeraj76766@gmail.com>
2026-01-06 22:24:38 +0530

    input

--- GeeksforGeeks/gfg.java
+++ GeeksforGeeks/gfg.java
@@ -3,8 +3,14 @@ import java.util.Scanner;
 public class gfg {
     public static void main(String args[]){
         Scanner sc =new Scanner(System.in);
-        System.out.println("Enter  a number:");
+        System.out.println("Enter  a Number Value:");
         int num = sc.nextInt();
+        System.out.println("Enter a String Value:");
+        String str = sc.nextLine();
+        System.out.println("Enter a Double Value:");
+        Double d = sc.nextDouble();
+        System.out.println("Double is: "+d);
+        System.out.println("String is: "+str);
         System.out.println("Number is: "+num);
     }
 }

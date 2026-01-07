4c1a84ee13fb2418a4127a6f9e2dbbfe7d189adf
Dheeraj Gowda YS <dheeraj76766@gmail.com>
2026-01-07 21:02:24 +0530

    restructuring

--- GeeksforGeeks/gfg.java
+++ GeeksforGeeks/gfg.java
@@ -6,11 +6,12 @@ public class gfg {
         System.out.println("Enter  a Number Value:");
         int num = sc.nextInt();
         System.out.println("Enter a String Value:");
-        String str = sc.nextLine();
+        String str = sc.next();
         System.out.println("Enter a Double Value:");
         Double d = sc.nextDouble();
         System.out.println("Double is: "+d);
         System.out.println("String is: "+str);
         System.out.println("Number is: "+num);
+        sc.close();
     }
 }

7e77da754f0a4cb61f54db5f79526fc21b5927a3
Dheeraj Gowda YS <dheeraj76766@gmail.com>
2026-01-07 23:53:31 +0530

    practice

---
+++ GeeksforGeeks/problem2.java
@@ -0,0 +1,16 @@
+package GeeksforGeeks;
+
+import java.util.Scanner;
+
+public class problem2 {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter A:");
+        int i = sc.nextInt();
+        System.out.print("Enter B:");
+        int j = sc.nextInt();
+        System.out.println("Sum: "+(i+j));
+        sc.close();
+    }
+}

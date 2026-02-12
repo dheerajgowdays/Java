426e2f24fe344bd856606e530e16ba7725370feb
Dheeraj Gowda YS <dheeraj76766@gmail.com>
2026-02-12 22:44:26 +0530

    sloving sorting problems

--- Stiver/Basic/Mergesort.java
+++ Stiver/Basic/Mergesort.java
@@ -1,27 +1,56 @@
 import java.util.Scanner;
 
 public class Mergesort {
-    public static int[] mergesort(int a,int low,int high){
-        if(low == high)
+    public static void merge(int a[],int low,int mid,int high){
+        int temp[] = new int[high-low+1];
+        int left = low;
+        int right = mid+1;
+        int k = 0;
+        while((left <= mid)&&(right <= high)){
+            if(a[left]<a[right]){
+                temp[k++] = a[left];
+                left++;
+            }
+            else{
+                temp[k++]=a[right];
+                right++;
+            }
+        }
+        while(left<=mid){
+            temp[k++] = a[left];
+            left++;
+        }
+        while(right<=high){
+            temp[k++]=a[right];
+            right++;
+        }
+        for(int i=low;i<=high;i++){
+            a[i] = temp[i-low];
+        } 
+    }
+    public static void mergesort(int a[],int low,int high){
+        if(low >= high)
             return;
         int mid = (low+high)/2;
         mergesort(a, low, mid);
         mergesort(a, mid+1, high);
-        merge(a,mid,low,high);
+        merge(a,low,mid,high);
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the size of an array:");
         int n = sc.nextInt();
         int a[] = new int[n];
-        for(int i=0;i<=n-1;i++){
+        for(int i=0;i<n;i++){
             System.out.println("Enter elments of array:");
             a[i] = sc.nextInt();
         }
         mergesort(a, 0, n-1);
-        for(int j=0;j<=n-1;j++){
-            System.out.println(a[j]+" ");
+        for(int j=0;j<n;j++){
+            System.out.print(a[j]+" ");
         }
+        System.out.println();
+        sc.close();
     }
     
 }

e5b6fcc59c85221a099efd205a3b35ec15a803f9
Dheeraj Gowda YS <dheeraj76766@gmail.com>
2026-06-24 21:38:19 +0530

    Sloving odd or even problem

--- GeeksforGeeks/logic-building-problems/OddorEven.java
+++ GeeksforGeeks/logic-building-problems/OddorEven.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 
 public class OddorEven{
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
+        Scanner scanner=new Scanner(System.in);
         System.out.println("Enter a Number: ");
-        int n = sc.nextInt();
+        int n = scanner.nextInt();
         
         if(n%2 == 0 ){
             System.out.printf("The %d is even%n",n);
@@ -13,6 +13,6 @@ public class OddorEven{
         else{
             System.out.printf("The %d is odd%n",n);
         }
-        sc.close();
+        scanner.close();
     }
 }
